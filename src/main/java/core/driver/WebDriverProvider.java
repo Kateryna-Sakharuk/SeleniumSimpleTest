df7ca4ba4a319c9417c1d1fe795ffea63d4030f3
Kateryna Saharuk <kateryna.saharuk@gmail.com>
2025-01-06 15:29:54 +0200

    delete web driver anager

--- src/main/java/core/driver/WebDriverProvider.java
+++ src/main/java/core/driver/WebDriverProvider.java
@@ -1,14 +1,15 @@
 package core.driver;
+
 import core.cache.TestCache;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.sql.DriverManager;
 import java.util.Objects;
+
 import static core.cache.TestCacheKey.BROWSER_NAME;
 
 public class WebDriverProvider implements IWebDriverProvider {
@@ -22,22 +23,13 @@ public class WebDriverProvider implements IWebDriverProvider {
     public WebDriver getWebDriver() {
         String browserName = TestCache.getStringValue(BROWSER_NAME);
         if (driver == null) {
-            boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
-            ChromeOptions options = new ChromeOptions();
-            if (headless) {
-                options.addArguments("--headless");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--window-size=1920x1080");
-            }
             switch (Objects.requireNonNull(browserName).toLowerCase()) {
                 case "chrome" -> {
                     log.info("Chrome driver selected");
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(options);
+                    driver = new ChromeDriver();
                 }
                 case "firefox" -> {
                     log.info("Firefox driver selected");
-                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                 }
                 default -> throw new IllegalArgumentException("Unsupported browser: " + browserName);
