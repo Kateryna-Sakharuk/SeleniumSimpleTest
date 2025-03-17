72ab78b157f7694d058169b29183bfd82e22f025
Kateryna Saharuk <kateryna.saharuk@gmail.com>
2025-03-17 17:14:00 +0200

    add cucumber dependency

---
+++ src/main/java/runner/CucumberTestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
+)
+public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+}
